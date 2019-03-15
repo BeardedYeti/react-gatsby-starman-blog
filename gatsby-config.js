a734f38da13ac9dec8d6eef794e8d420990e2d84
Colin Smith <cmsgoboston41@gmail.com>
2019-03-15 14:07:48 -0400

    environment setup

---
+++ gatsby-config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby-Starman-Starter',
+    author: {
+      name: 'Colin Smith',
+      email: 'cmsgoboston41@gmail.com',
+      telegram: '#',
+      twitter: '@thecolinsmith',
+      github: 'BeardedYeti',
+      rss: '#',
+      vk: '#'
+    },
+    subtitle: 'Gatsby-Starman',
+  },
+  plugins: [
+    'gatsby-plugin-netlify-cms',
+    {
+      resolve: 'gatsby-plugin-netlify',
+      options: {
+        headers: {
+          '/*.js': [
+            'Cache-Control: public, max-age=0, must-revalidate'
+          ],
+          '/**/*.html': [
+            'Cache-Control: public, max-age=0, must-revalidate'
+          ],
+        }, // option to add more headers. `Link` headers are transformed by the below criteria
+        allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
+        mergeSecurityHeaders: true, // boolean to turn off the default security headers
+        mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
+        mergeCachingHeaders: true, // boolean to turn off the default caching headers
+        transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
+        generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
+      },
+    }
+  ],
+};
